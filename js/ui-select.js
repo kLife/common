50ed197c54ed89a7e15d974f3b083045b5dafd26
Kazuki Yamada <koukun0120@gmail.com>
2016-12-05 21:51:50 +0900

    選択肢をホバーするとその文字が確認できるように

--- js/ui-select.js
+++ js/ui-select.js
@@ -3,7 +3,7 @@
 // ui-select.js
 // - UI_Select
 (function (undefined) {
-	
+
 	/* -------------------------------------------------- */
 	// UI_Select Class
 	// セレクトタグっぽいの
@@ -19,7 +19,7 @@
 			openListenerType: "click", // click/hover
 			onClick_option: function(){}
 		};
-		
+
 		groupsData = [
 			{
 				groupName: "ステータス上昇",
@@ -35,12 +35,12 @@
 			}, ...
 		];
 	*/
-	
+
 	function UI_Select(options, groupsData) {
 		// Constructor
 		this.options = $.extend({}, UI_Select.defaults, options);
 		this.groupsData = groupsData;
-		
+
 		this.$container = null;
 		this.$select = null;
 		this.$selectText = null;
@@ -51,16 +51,16 @@
 		this.$pageText = null;
 		this.$btnPageLeft = null;
 		this.$btnPageRight = null;
-		
+
 		this.isOpen = false;
 		this.indexTable = {};
 		this.firstOptionKey = undefined;
 		this.selectedKey = undefined;
 		this.pageNumber = 0;
-		
+
 		this._create();
 	}
-	
+
 	// Static members
 	$.extend(UI_Select, {
 		defaults: {
@@ -78,63 +78,63 @@
 		},
 		documentClickHandlers: [],
 	});
-	
+
 	// Instance members
 	UI_Select.prototype = {
-		
+
 		getContainer: function() {
 			return this.$container;
 		},
-		
+
 		getIndexData: function(key) {
 			if (key === undefined) {
 				return null
 			}
-			
+
 			return this.indexTable[key];
 		},
-		
+
 		getElem: function(key) {
 			var indexData = this.getIndexData(key);
-			
+
 			if (indexData) {
 				return this.$list
 					.find("li.ui-select-group").eq(indexData.groupIndex)
 					.find("li").eq(indexData.optionIndex);
 			}
 		},
-		
+
 		getOption: function(key) {
 			var indexData = this.getIndexData(key);
-			
+
 			if (indexData) {
 				return this.groupsData[indexData.groupIndex].options[indexData.optionIndex];
 			}
 		},
-		
+
 		getSelectedElem: function() {
 			return this.getElem(this.selectedKey);
 		},
-		
+
 		getSelectedOption: function() {
 			return this.getOption(this.selectedKey);
 		},
-		
+
 		getSelectedValue: function() {
 			var option = this.getSelectedOption()
 			return option ? option.value : "";
 		},
-		
+
 		setSelectText: function(value) {
 			this.$selectText.html(value);
 		},
-		
+
 		reset: function(options, groupsData) {
 			this.options = $.extend({}, this.options, options);
 			this.groupsData = groupsData;
-			
+
 			this._createListInner();
-			this.$selectText.html(this.options.initText);
+			this.setSelectText(this.options.initText)
 			this.$selectIcon.text(this.options.selectIcon);
 			this.$select.css("width", this.options.selectWidth);
 			this.$list.css({
@@ -142,10 +142,10 @@
 				marginLeft: this.options.listMarginLeft
 			});
 		},
-		
+
 		select: function(key) {
 			var indexes;
-			
+
 			if (key === undefined) {
 				if (this.firstOptionKey !== undefined) {
 					key = this.firstOptionKey;
@@ -153,9 +153,9 @@
 					return;
 				}
 			}
-			
+
 			indexes = this.indexTable[key];
-			
+
 			if (indexes) {
 				this.$list
 					.find("li.ui-select-group").eq(indexes.groupIndex)
@@ -166,11 +166,11 @@
 				Koukun.fn.log("Error[UI_Select.select]: indexTable[" + key +"] is not defined.");
 			}
 		},
-		
+
 		reselect: function() {
 			this.select(this.selectedKey);
 		},
-		
+
 		open: function() {
 			if (!this.isOpen) {
 				this.isOpen = true;
@@ -178,7 +178,7 @@
 				this.$select.addClass("ui-select-open");
 			}
 		},
-		
+
 		close: function() {
 			if (this.isOpen) {
 				this.isOpen = false;
@@ -186,19 +186,19 @@
 				this.$select.removeClass("ui-select-open");
 			}
 		},
-		
+
 		leftPage: function() {
 			if (this.pageNumber > 0) {
 				this.turnPage(this.pageNumber - 1);
 			}
 		},
-		
+
 		rightPage: function() {
 			if (this.pageNumber < this.$grounps.length - 1) {
 				this.turnPage(this.pageNumber + 1);
 			}
 		},
-		
+
 		turnPage: function(pageNumber) {
 			if (this.options.isPageView) {
 				this.pageNumber = pageNumber;
@@ -208,13 +208,13 @@
 				this.$btnPageLeft.toggleClass("ui-select-page-disable", this.pageNumber <= 0);
 			}
 		},
-		
+
 		_create: function() {
 			this._createFrame();
 			this._createListInner();
 			this._setEventListener();
 		},
-		
+
 		_createFrame: function() {
 			this.$container = $("<div>").addClass("ui-select");
 			this.$select = $("<div>").addClass("ui-select-select").css("width", this.options.selectWidth);
@@ -225,16 +225,16 @@
 				marginLeft: this.options.listMarginLeft
 			});
 			this.$groupWrapper = $("<div>").addClass("ui-select-wrapper");
-			
+
 			this.$select.append(this.$selectText, this.$selectIcon);
 			this.$list.html(this.$groupWrapper);
 			this.$container.append(this.$select, this.$list);
-			
+
 			// Setting panel-view
 			if (this.options.isPanelView) {
 				this.$list.addClass("ui-select-panel-view");
 			}
-			
+
 			// create page form
 			if (this.options.isPageView) {
 				var $pageForm = $("<div class='ui-select-page-form'>");
@@ -243,37 +243,38 @@
 				this.$btnPageRight = $("<div class='ui-select-page-right'>").text("次 >");
 				this.pageNumber = 0;
 				$pageForm.append([this.$btnPageLeft, this.$pageText, this.$btnPageRight]);
-				
+
 				this.options.isPageBtnOnTop ?
 				 	this.$list.prepend($pageForm) :
 				 	this.$list.append($pageForm);
 			}
 		},
-		
+
 		_createListInner: function() {
 			var strGroups = "";
 			var i, j, groupName, options, option, strType, strOpContainer, strOptions, strOpIcon;
-			
+
 			this.indexTable = {};
 			this.firstOptionKey = undefined;
 			this.selectedKey = undefined;
-			
+
 			// 高速化？
+			// フラグメント使う
 			for (i = 0; i < this.groupsData.length; i++) {
 				groupName = this.groupsData[i].groupName;
 				options = this.groupsData[i].options;
 				strOptions = "";
-				
+
 				if (typeof groupName === "string" && groupName !== "") {
 					strType = "<div class='ui-select-type'>" + groupName + "</div>";
 				} else {
 					strType = "";
 				}
-				
+
 				if (options.length > 0) {
 					this.firstOptionKey = this.firstOptionKey || options[0].key;
 				}
-				
+
 				for (j = 0; j < options.length; j++) {
 					option = options[j];
 					strOpIcon = option.icon ? option.icon + " " : "";
@@ -284,13 +285,13 @@
 					strOptions += "<li style='width:" + this.options.optionWidth + "px'>" +
 						strOpIcon + " " + option.value + "</li>";
 				}
-				
+
 				strOpContainer = "<ul class='ui-select-options'>" + strOptions + "</ul>";
 				strGroups += "<li class='ui-select-group'>" + strType + strOpContainer + "</li>";
 			}
-			
+
 			this.$groupWrapper.html(strGroups)
-			
+
 			if (this.options.isPageView) {
 				this.$grounps = this.$list.find(".ui-select-group");
 				this.$pageText.text((this.pageNumber + 1) + "/" + this.$grounps.length);
@@ -298,10 +299,10 @@
 				this.turnPage(0);
 			}
 		},
-		
+
 		_setEventListener: function() {
 			var that = this;
-			
+
 			// List open/close
 			switch (this.options.openListenerType) {
 				case "click":
@@ -322,7 +323,7 @@
 					});
 					break;
 			}
-			
+
 			// option click
 			this.$list.on("click", "ul li", function(ev, isTriggered) {
 				var $this = $(this);
@@ -331,25 +332,37 @@
 				var groupIndex = $group.index();
 				var option = that.groupsData[groupIndex].options[optionIndex];
 				var $selected = that.getSelectedElem();
-				
+
 				$selected && $selected.removeClass("ui-select-selected");
 				$this.addClass("ui-select-selected");
-				
+
 				that.selectedKey = option.key;
-				that.$selectText.html(option.value);
+				that.setSelectText(option.value);
+				that.setSelectText(that.getSelectedValue());
 				that.close();
 				that.options.onClick_option(option.key, option.value, isTriggered);
 			});
-			
+
+			// option hover
+			this.$list.on("mouseenter", "ul li", function(ev, isTriggered) {
+				var $this = $(this);
+				var $group = $this.parent().parent();
+				var option = that.groupsData[$group.index()].options[$this.index()];
+
+				that.setSelectText(option.value);
+			}).on("mouseleave", "ul li", function(ev, isTriggered) {
+				that.setSelectText(that.getSelectedValue());
+			});
+
 			// page form
 			if (this.options.isPageView) {
 				this.$btnPageLeft.on("click", $.proxy(this.leftPage, this));
 				this.$btnPageRight.on("click", $.proxy(this.rightPage, this));
 			}
 		}
-		
+
 	}
-	
+
 	$(document).ready(function() {
 		$(document).on("click", function(e) {
 			$.each(Koukun.cl.UI_Select.documentClickHandlers, function(index, handler) {
@@ -357,7 +370,7 @@
 			});
 		});
 	});
-	
+
 	$.extend(Koukun.cl, {
 		UI_Select: UI_Select
 	});
